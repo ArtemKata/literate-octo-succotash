815c859f5ecda35dd901f827536394f1589fc394
ArtemKata <133988057+ArtemKata@users.noreply.github.com>
2023-05-18 22:03:41 +0700

    Add files via upload

---
+++ calc_kata/src/main/java/org/example/Test.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Test {
+    public static void main(String[] args) {
+        int  a = 9;
+        int b = 32;
+        System.out.println(a+b);
+    }
+}
+// map
